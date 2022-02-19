2b7c99519f427a505898fb1382a2d553c97e96b4
Jarosław Cielecki <jarekcielecki15@gmail.com>
2022-02-19 17:46:34 +0100

    Add Details Page

--- lib/repositories/items_respository.dart
+++ lib/repositories/items_respository.dart
@@ -21,11 +21,19 @@ class ItemsRepository {
     });
   }
 
-  Future<void> delete({required String id})  {
-   return FirebaseFirestore.instance
-          .collection('items')
-          .doc(id)
-          .delete();
+  Future<void> delete({required String id}) {
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
+
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      imageUrl: doc['image_url'],
+      relaseData: (doc['release_date'] as Timestamp).toDate(),
+    );
   }
 
   Future<void> add(
@@ -33,12 +41,12 @@ class ItemsRepository {
     String imageURL,
     DateTime releaseDate,
   ) async {
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'image_url': imageURL,
-          'release_date': releaseDate,
-        },
-      );
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'image_url': imageURL,
+        'release_date': releaseDate,
+      },
+    );
   }
 }
